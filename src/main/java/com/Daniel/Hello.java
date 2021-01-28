9c992045c46494136c66f3dabada3d8e75f0a161
Daniel Chang <changdannyb107@gmail.com>
2021-01-28 15:43:40 +0800

    Java and kotlin

--- src/main/java/com/Daniel/Hello.java
+++ src/main/java/com/Daniel/Hello.java
@@ -6,5 +6,14 @@ public class Hello {
         //new Person().hello();
         Person p=new Person();
         p.hello();
-    }
+        Integer age = 10;
+        age.byteValue();
+        //char c = '我';
+        byte b = 120;
+        float weight =66.5f;
+        float height = 1.7f;
+        boolean adult =true;
+        boolean enroll = false;
+        String name = "Daniel";
+ }
 }
--- src/main/kotlin/com/Daniel/Hello.kt
+++ src/main/kotlin/com/Daniel/Hello.kt
@@ -5,6 +5,10 @@ fun main() {
     //Human().hello()
     val h=Human()
     h.hello();
+    val age=10; //val 定義不會改變的值
+    //val age : Int =10 分號後可自行指定資料型態
+    var weight = 66.5 //var 定義可改變的值
+    var name : String //只定義不給值時需冒號並補上資料型態
 }
 
 
